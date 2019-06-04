36fc2690dcc38c074727736e9959e187be520dd7
matko-antunovic <47244433+matko-antunovic@users.noreply.github.com>
2019-06-04 17:01:37 +0200

    Updates

---
+++ precache-manifest.9ec84e828a4e124ee714fb68369f1110.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f40393c912e497d35ca488cbe55b11f9",
+    "url": "/moviebaseg/index.html"
+  },
+  {
+    "revision": "9694bc1624885e4519e3",
+    "url": "/moviebaseg/static/css/main.8d3c797e.chunk.css"
+  },
+  {
+    "revision": "538d812bc7240d31be6b",
+    "url": "/moviebaseg/static/js/2.088e430b.chunk.js"
+  },
+  {
+    "revision": "9694bc1624885e4519e3",
+    "url": "/moviebaseg/static/js/main.c4315f80.chunk.js"
+  },
+  {
+    "revision": "6db389b8b9d0fc67ce27",
+    "url": "/moviebaseg/static/js/runtime~main.9ae4f4e5.js"
+  }
+]);
